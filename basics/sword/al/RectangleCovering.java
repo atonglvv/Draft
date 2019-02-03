0df1586e4c1abe6d25c6162a50e6fa9d56f72935
Innovation <lt1550919167@gmail.com>
2019-02-03 14:18:27 +0800

    swap al 矩形覆盖问题

---
+++ basics/sword/al/RectangleCovering.java
@@ -0,0 +1,27 @@
+package basics.sword.al;
+
+/**
+ * @Auther: carver
+ * @Date: 2019/2/3 14:15
+ * @email lt1550919167@163.com
+ * @QQ 1550919167
+ * @Description: 问题描述：
+ *              我们可以用 2*1 的小矩形横着或者竖着去覆盖更大的矩形。
+ *              请问用 n 个 2*1 的小矩形无重叠地覆盖一个 2*n 的大矩形，
+ *              总共有多少种方法？
+ */
+public class RectangleCovering {
+    public int pectCover(int n) {
+        if (n <= 2) {
+            return n;
+        }
+        int pre1 = 1,pre2 = 2;
+        int result = 0;
+        for (int i = 3; i < n; i++) {
+            result = pre1 + pre2;
+            pre1 = pre2;
+            pre2 = result;
+        }
+        return result;
+    }
+}
