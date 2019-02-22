21e85c9538f9da8116facb02b81c8d227841835d
Innovation <lt1550919167@gmail.com>
2019-02-22 14:43:59 +0800

    ClassLoading Child and Parent

---
+++ basics/jvm/classLoading/Child.java
@@ -0,0 +1,18 @@
+package basics.jvm.classLoading;
+
+/**
+ * @Auther: carver
+ * @Date: 2019/2/22 14:40
+ * @email lt1550919167@163.com
+ * @QQ 1550919167
+ * @Description: JVM类加载时机：当初始化一个类时，如果发现其父类还没有进行初始化，则需要先触发其父类的初始化
+ */
+public class Child extends Parent{
+    static {
+        System.out.println("Child 初始化。。。");
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
