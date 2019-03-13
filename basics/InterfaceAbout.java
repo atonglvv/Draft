c3f6ce785cfa88344308b71c5c340d2186edc21a
Innovation <lt1550919167@gmail.com>
2019-03-13 09:56:52 +0800

    AdditionOfLargeNumbers

---
+++ basics/InterfaceAbout.java
@@ -0,0 +1,13 @@
+package basics;
+
+/**
+ * @Auther: carver
+ * @Date: 2019/3/5 22:46
+ * @email lt1550919167@163.com
+ * @QQ 1550919167
+ * @Description:
+ */
+public interface InterfaceAbout {
+
+    void fun();
+}
