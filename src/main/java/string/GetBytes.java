beb472629a35050e7af5b03303f559d054848498
atong <lt1550919167@163.com>
2020-12-18 16:25:37 +0800

    feat String.getBytes

---
+++ src/main/java/string/GetBytes.java
@@ -0,0 +1,26 @@
+package string;
+
+import java.io.UnsupportedEncodingException;
+
+/**
+ * @program: draft
+ * @description: getBytes method 相关操作
+ * @author: atong
+ * @create: 2020-12-18 16:19
+ */
+public class GetBytes {
+
+    /**
+     * 获取String在某一字符集下的长度
+     * @param args
+     * @throws UnsupportedEncodingException
+     */
+    public static void main(String[] args) throws UnsupportedEncodingException {
+        String a = "123abc";
+        int num = a.getBytes("utf-8").length;
+        System.out.println(num);
+        a = "中文";
+        num = a.getBytes("utf-8").length;
+        System.out.println(num);
+    }
+}
