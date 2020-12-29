b986015784554d177e81127cd2b4c643977b28a5
atong <lt1550919167@163.com>
2020-12-29 19:44:07 +0800

    spring source commit

--- src/main/java/framework/spring/SpringAnnotationConfig.java
+++ src/main/java/framework/spring/SpringAnnotationConfig.java
@@ -1,6 +1,7 @@
 package framework.spring;
 
 import framework.spring.bean.BeanTest;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
@@ -13,9 +14,9 @@ import org.springframework.context.annotation.Configuration;
  */
 @Configuration
 @ComponentScan(basePackages = {"framework.spring"})
-public class SpringMain {
+public class SpringAnnotationConfig {
     public static void main(String[] args) {
-        AnnotationConfigApplicationContext aac = new AnnotationConfigApplicationContext();
+        AnnotationConfigApplicationContext aac = new AnnotationConfigApplicationContext(SpringAnnotationConfig.class);
         BeanTest bean = aac.getBean(BeanTest.class);
         System.out.println(bean);
     }
