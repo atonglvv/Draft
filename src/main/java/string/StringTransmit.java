b3e0d1efe63670c657862da8ff50daec79445963
atong <lt1550919167@163.com>
2021-01-21 11:10:33 +0800

    feat StringTransmit

---
+++ src/main/java/string/StringTransmit.java
@@ -0,0 +1,33 @@
+package string;
+
+/**
+ * @program: draft
+ * @description: String传递
+ * @author: atong
+ * @create: 2021-01-20 17:41
+ */
+public class StringTransmit {
+    public static void main(String[] args) {
+        String a = "aaa";
+        String b = "bbb";
+        String c = "ccc";
+
+        transmit(a,b,c);
+
+        transmit(a);
+
+        System.out.println(a + b + c);
+    }
+
+    public static boolean transmit (String... strs) {
+        for (String str : strs) {
+            str = "ddd";
+        }
+        return false;
+    }
+
+    public static boolean transmit (String str) {
+        str = "ddd";
+        return false;
+    }
+}
