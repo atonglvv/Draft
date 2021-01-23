19961d9a177dafd93e807d2ee2d54aa5863db177
atongTs <atongLv@163.com>
2021-01-23 14:17:44 +0800

    rpc1.0 add entity

---
+++ src/main/java/framework/rpc/entity/User.java
@@ -0,0 +1,18 @@
+package framework.rpc.entity;
+
+import lombok.Data;
+
+/**
+ * @program: draft
+ * @description: 用户实体类
+ * @author: atong
+ * @create: 2021-01-23 14:11
+ */
+@Data
+public class User {
+    private Long id;
+    /** 姓名 */
+    private String name;
+    /** 年龄 */
+    private Integer age;
+}
