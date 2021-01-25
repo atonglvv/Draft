47561a9b695eb60dedfea19aa12c67e9bc847b3d
atongTs <atongLv@163.com>
2021-01-25 20:11:11 +0800

    rpc2.0,客户端屏蔽stock逻辑,只关注业务

---
+++ src/main/java/framework/rpc/rpcb/client/Stub.java
@@ -0,0 +1,40 @@
+package framework.rpc.rpcb.client;
+
+import framework.rpc.domain.entity.User;
+
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+
+/**
+ * @program: draft
+ * @description: rpc 客户端 stock处理流程提取
+ * @author: atong
+ * @create: 2021-01-25 19:47
+ */
+public class Stub {
+    public User getUser(Integer id) {
+        try {
+            //1.建立远程连接
+            Socket socket = new Socket("127.0.0.1",8888);
+            //2.服务端参数
+            ObjectOutputStream outputStream = new ObjectOutputStream(socket.getOutputStream());
+            outputStream.writeLong(id);
+            outputStream.flush();
+
+            //3.接收服务端返回的结果
+            ObjectInputStream inputStream = new ObjectInputStream(socket.getInputStream());
+            User user = (User) inputStream.readObject();
+
+            //资源关闭
+            inputStream.close();
+            outputStream.close();
+            socket.close();
+
+            return user;
+        }catch (Exception e) {
+            e.printStackTrace();
+            return new User();
+        }
+    }
+}
---
+++ src/main/java/framework/rpc/rpcb/server/RpcServer.java
@@ -0,0 +1,44 @@
+package framework.rpc.rpcb.server;
+
+import framework.rpc.domain.entity.User;
+import framework.rpc.service.UserService;
+import framework.rpc.service.impl.UserServiceImpl;
+
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+/**
+ * @program: draft
+ * @description: Rpc服务端
+ * @author: atong
+ * @create: 2021-01-22 11:41
+ */
+public class RpcServer {
+    public static void main(String[] args) {
+        try {
+            ServerSocket serverSocket = new ServerSocket(8888);
+            while (true){
+                Socket socket = serverSocket.accept();
+                //处理网络IO
+                process(socket);
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    public static void process (Socket socket) throws IOException {
+        //获取接受的参数
+        ObjectInputStream inputStream = new ObjectInputStream(socket.getInputStream());
+        Long userId = inputStream.readLong();
+        //输出result
+        ObjectOutputStream outputStream = new ObjectOutputStream(socket.getOutputStream());
+        UserService userService = new UserServiceImpl();
+        User user = userService.getUser(userId);
+        outputStream.writeObject(user);
+        outputStream.flush();
+    }
+}
