5d7c8f01c4e07a37082ed78ffda6ef0a421fbbfb
atong <lt1550919167@163.com>
2021-01-29 17:32:35 +0800

    feat leetcode.ThreeSumColsest

--- src/main/java/sword/leetcode/linearlist/array/ThreeSumClosest.java
+++ src/main/java/sword/leetcode/linearlist/array/ThreeSumClosest.java
@@ -1,5 +1,7 @@
 package sword.leetcode.linearlist.array;
 
+import java.util.Arrays;
+
 /**
  * @program: draft
  * @description:
@@ -11,9 +13,46 @@ package sword.leetcode.linearlist.array;
  *
  * Solution:
  * 先排序,然后左右夹逼,时间复杂度O(n²)
+ * 该题注意Closest,即为 Math.abs(sum - target)最小值。
+ *
  * @author: atong
  * @create: 2021-01-29 15:29
  */
 public class ThreeSumClosest {
 
+    public static void main(String[] args) {
+        int[] nums = {-1, 2, 1, -4};
+        int result = threeSumClosest(nums, 1);
+        System.out.println(result);
+    }
+
+    public static int threeSumClosest (int[] nums, int target) {
+        int minGap = Integer.MAX_VALUE;
+        int result = 0;
+        //先排序
+        Arrays.sort(nums);
+        for (int i = 0; i < nums.length; i++) {
+            int low = i + 1;
+            int high = nums.length - 1;
+            while (low < high) {
+                int sum = nums[i] + nums[low] + nums[high];
+                int gap = Math.abs(sum - target);
+                if (gap < minGap) {
+                    minGap = gap;
+                    result = sum;
+                }
+                /**
+                 * 注意该处,夹逼方式跟ThreeSum不同。
+                 * 比较sum 跟 target的大小
+                 * 若sum比target小则将第二个指针low往右移一位,否则high往左移一位
+                 */
+                if (sum < target) {
+                    low++;
+                }else {
+                    high--;
+                }
+            }
+        }
+        return result;
+    }
 }
