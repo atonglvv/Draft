3a6fdc6555e2e6b86a1e2e56bb0282520a2d8a27
atong <lt1550919167@163.com>
2021-01-29 16:35:41 +0800

    feat leetcode.ThreeSumClosest

---
+++ src/main/java/sword/leetcode/linearlist/array/ThreeSumClosest.java
@@ -0,0 +1,19 @@
+package sword.leetcode.linearlist.array;
+
+/**
+ * @program: draft
+ * @description:
+ * Three Sum Closest[adj. 最靠近的]
+ * Given an array S of n integers, find three integers in S such that the sum is closest to a given number,target.
+ * Return the sum of the three integers. You may assume that each input would have exactly one solution.
+ * For example, given array S = {-1 2 1 -4}, and target = 1.
+ * The sum that is closest to the target is 2. (-1 + 2 + 1 = 2).
+ *
+ * Solution:
+ * 先排序,然后左右夹逼,时间复杂度O(n²)
+ * @author: atong
+ * @create: 2021-01-29 15:29
+ */
+public class ThreeSumClosest {
+
+}
--- src/main/java/sword/leetcode/linearlist/array/leetcodeLinearListArray.md
+++ src/main/java/sword/leetcode/linearlist/array/leetcodeLinearListArray.md
@@ -1,1 +1,5 @@
 # Leetcode LinearList Array
+
+
+未完成：
+MedianOfTwoSortedArrays
