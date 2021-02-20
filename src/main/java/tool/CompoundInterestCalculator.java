91395871d708ffd825e678d32c7d547da0a5b0f4
atong <lt1550919167@163.com>
2021-02-20 11:18:31 +0800

    复利计算器

---
+++ src/main/java/tool/CompoundInterestCalculator.java
@@ -0,0 +1,28 @@
+package tool;
+
+/**
+ * @program: draft
+ * @description: Compound Interest Calculator
+ * @author: atong
+ * @create: 2021-02-20 10:52
+ */
+public class CompoundInterestCalculator {
+    private static final double income = 120000;
+
+    public static void main(String[] args) {
+        double rate = 0.05;
+        int year = 4;
+        double incomex = income(income, rate, year);
+        System.out.println(incomex);
+    }
+
+    public static double income(double money, double rate, int year) {
+        double result = 0;
+        result = money + money * rate;
+        if (year == 1) {
+            return result;
+        }
+        //每年攒income
+        return income(result + income, rate, year - 1);
+    }
+}
