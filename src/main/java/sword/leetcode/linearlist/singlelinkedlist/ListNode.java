9caefe2789d5173fe14276ac8d80b5d126198605
atong <lt1550919167@163.com>
2021-02-26 10:06:16 +0800

    feat leetcode.AddTwoNumbers

---
+++ src/main/java/sword/leetcode/linearlist/singlelinkedlist/ListNode.java
@@ -0,0 +1,14 @@
+package sword.leetcode.linearlist.singlelinkedlist;
+
+/**
+ * @program: draft
+ * @description:
+ * Definition for singly-linked list.
+ * @author: atong
+ * @create: 2021-02-26 09:32
+ */
+public class ListNode {
+    int val;
+    ListNode next;
+    ListNode(int x) { val = x; }
+}
