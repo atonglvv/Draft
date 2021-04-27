cc61f5ace03594d97f02ef15085f2cef97915a12
atong <lt1550919167@163.com>
2021-04-27 14:48:59 +0800

    draft

---
+++ src/main/java/basics/DDDraft.java
@@ -0,0 +1,13 @@
+package basics;
+
+/**
+ * @program: draft
+ * @description:
+ * @author: atong
+ * @create: 2021-04-26 14:43
+ */
+public class DDDraft {
+    public static void main(String[] args) {
+
+    }
+}
