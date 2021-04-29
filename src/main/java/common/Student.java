c8a1938f7307cded19970143b3bc82981ca379fe
atongTs <atongLv@163.com>
2021-04-29 20:42:49 +0800

    commit

--- src/main/java/common/Student.java
+++ src/main/java/common/Student.java
@@ -12,6 +12,8 @@ public class Student {
     private String name;
     private int age;
 
+    public Student() {}
+
     public Student(String name, int age) {
         this.name = name;
         this.age = age;
