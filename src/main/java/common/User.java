9726db91e023c323f03fff1fa64c0397ff896169
atongTs <atongLv@163.com>
2021-05-09 11:16:49 +0800

    perf User lombok

--- src/main/java/common/User.java
+++ src/main/java/common/User.java
@@ -1,5 +1,8 @@
 package common;
 
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
 import java.io.Serializable;
 
 /**
@@ -8,33 +11,12 @@ import java.io.Serializable;
  * @author: atong
  * @create: 2021-01-26 14:31
  */
+@AllArgsConstructor
+@NoArgsConstructor
 public class User implements Serializable {
     private Long id;
     /** 姓名 */
     private String name;
     /** 年龄 */
     private Integer age;
-
-    public User(Long id, String name, Integer age) {
-        this.id = id;
-        this.name = name;
-        this.age = age;
-    }
-
-    public User(Long id, String name) {
-        this.id = id;
-        this.name = name;
-    }
-
-    public User(String name, Integer age) {
-        this.name = name;
-        this.age = age;
-    }
-
-    public User(Long id, Integer age) {
-        this.id = id;
-        this.age = age;
-    }
-
-    public User() {}
 }
