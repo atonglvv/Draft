e89d6ff897ffda7c005f7acf6d12aa0f1353ac8f
atongTs <atongLv@163.com>
2021-05-09 14:38:33 +0800

    perf Fields

--- src/main/java/reflect/main/Fields.java
+++ src/main/java/reflect/main/Fields.java
@@ -19,10 +19,30 @@ public class Fields {
     }
 
     public static void getFields (Class cl) {
+        /**
+         * getDeclaredFields
+         * Returns an array of {@code Field} objects reflecting all the fields
+         * declared by the class or interface represented by this
+         * {@code Class} object.
+         * This includes public, protected, default (package) access, and private fields,
+         * but excludes inherited[遗传的;继承权的] fields.
+         *
+         * <p> If this {@code Class} object represents a class or interface with no
+         * declared fields, then this method returns an array of length 0.
+         *
+         * <p> If this {@code Class} object represents an array type, a primitive
+         * type, or void, then this method returns an array of length 0.
+         *
+         * <p> The elements in the returned array are not sorted and are not in any
+         * particular order.
+         */
         Field[] declaredFields = cl.getDeclaredFields();
         for (Field field : declaredFields) {
+            //方法返回一个Class对象，用于标识此Field对象所表示的字段的声明类型。
             Class<?> type = field.getType();
+            //方法返回此Field对象表示的字段的名称。
             String name = field.getName();
+            //返回Field修饰符
             String modifiers = Modifier.toString(field.getModifiers());
 
             System.out.println("Fields Type = " + type);
