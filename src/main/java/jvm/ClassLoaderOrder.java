d36d9ecf3a20d223f3f5297d9b8ff1e8880c61cc
atongTs <atongLv@163.com>
2021-05-15 20:30:24 +0800

    perf ClassLoaderOrder

--- src/main/java/jvm/ClassLoaderOrder.java
+++ src/main/java/jvm/ClassLoaderOrder.java
@@ -10,7 +10,15 @@ package jvm;
 public class ClassLoaderOrder {
 
     public static void main(String[] args) {
+        /**
+         * new B() -> 第一次加载B类, B extends A 所以需要先加载A类。
+         * 先加载父类(A)的静态代码块,然后加载子类(B)的静态代码块
+         * 然后父类构造函数创建对象, 最后子类构造函数
+         */
         A ab = new B();
+        System.out.println();
+        System.out.println("============");
+        //第二次 new B(), AB都已经加载到JVM中, 不再需要加载。
         ab = new B();
     }
 
