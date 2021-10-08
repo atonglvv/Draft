82949e15134411516006d1490618e985db160c3d
atong <lt1550919167@163.com>
2021-10-08 18:29:36 +0800

    feat: operator-Shifting

---
+++ src/main/java/basics/operator/shifting/Shifting.java
@@ -0,0 +1,15 @@
+package basics.operator.shifting;
+
+/**
+ * @program: draft
+ * @description: 移位运算符
+ * @author: atong
+ * @create: 2021-10-08 11:55
+ */
+public class Shifting {
+    public static void main(String[] args) {
+        int a = 5;
+        System.out.println("a >> 2 = " + (a >> 2));
+        System.out.println("a << 2 = " + (a << 2));
+    }
+}
