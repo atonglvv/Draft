dc5d0e28eda27818fe8630fb5876745d7970a593
atongTs <atongLv@163.com>
2021-10-08 21:39:21 +0800

    feat: java.lang.String

---
+++ src/main/java/java/lang/String.java
@@ -0,0 +1,14 @@
+package java.lang;
+
+/**
+ * @program: draft
+ * @description: 自定义String
+ * jvm保护机制,禁止执行，双亲委派机制
+ * @author: atong
+ * @create: 2021-10-08 21:30
+ */
+public class String {
+    public static void main(String[] args) {
+        System.out.println("Hello,String");
+    }
+}
