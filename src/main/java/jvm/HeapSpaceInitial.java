cf0b3be2eb36943fcfd6fd9dd439072ffae7152a
atongTs <atongLv@163.com>
2021-10-20 22:51:48 +0800

    feat: HeapSpaceInitial

---
+++ src/main/java/jvm/HeapSpaceInitial.java
@@ -0,0 +1,19 @@
+package jvm;
+
+/**
+ * @program: draft
+ * @description: 堆内存大小
+ * @author: atong
+ * @create: 2021-10-20 22:47
+ */
+public class HeapSpaceInitial {
+    public static void main(String[] args) {
+        //返回Java虚拟机中的堆内存总量
+        long initialMemory = Runtime.getRuntime().totalMemory() / 1024 / 1024;
+        //返回Java虚拟机试图使用的最大堆内存容量
+        long maxMemory = Runtime.getRuntime().maxMemory() / 1024 / 1024;
+
+        System.out.println("-Xms : " + initialMemory + "M");
+        System.out.println("-Xmx : " + maxMemory + "M");
+    }
+}
