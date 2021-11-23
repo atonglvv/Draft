ef0967d70a2ade086a783ea85ad41736b42e3fc0
atongTs <atongLv@163.com>
2021-11-23 23:10:13 +0800

    fix: rename String -> Stringa

--- src/main/java/java/lang/Stringa.java
+++ src/main/java/java/lang/Stringa.java
@@ -4,10 +4,11 @@ package java.lang;
  * @program: draft
  * @description: 自定义String
  * jvm保护机制,禁止执行，双亲委派机制
+ * 此处因为影响其他import String的代码，故将其改名为Stringa
  * @author: atong
  * @create: 2021-10-08 21:30
  */
-public class String {
+public class Stringa {
     public static void main(String[] args) {
         System.out.println("Hello,String");
     }
