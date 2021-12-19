06ee45709a0c068095d5d707dbc93ef3b508764c
atongTs <atongLv@163.com>
2021-12-19 22:29:54 +0800

    feat: CopyOnWriteSafe

---
+++ src/main/java/thread/collection/CopyOnWriteSafe.java
@@ -0,0 +1,29 @@
+package thread.collection;
+
+import java.util.UUID;
+import java.util.concurrent.CopyOnWriteArrayList;
+
+/**
+ * @program: draft
+ * @description: CopyOnWriteArrayList线程安全的
+ * 写时复制
+ * @author: atong
+ * @create: 2021-12-19 22:20
+ */
+public class CopyOnWriteSafe {
+    public static void main(String[] args) {
+        CopyOnWriteArrayList<String> list = new CopyOnWriteArrayList<>();
+        for (int i = 0; i < 30; i++) {
+            new Thread(()->{
+                /*
+                 * 多线程 添加 读取 会报一下异常
+                 * java.util.ConcurrentModificationException
+                 */
+                //添加元素
+                list.add(UUID.randomUUID().toString().substring(0, 8));
+                //读取元素
+                System.out.println(list);
+            }, String.valueOf(i)).start();
+        }
+    }
+}
