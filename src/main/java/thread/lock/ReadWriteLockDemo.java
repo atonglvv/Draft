50b6ca8169f1660350e0af2b694fffa9fd039830
atongTs <atongLv@163.com>
2021-12-27 22:07:25 +0800

    style: log

--- src/main/java/thread/lock/ReadWriteLockDemo.java
+++ src/main/java/thread/lock/ReadWriteLockDemo.java
@@ -7,6 +7,10 @@ import java.util.concurrent.locks.ReentrantReadWriteLock;
 /**
  * @program: draft
  * @description: ReadWriteLock 实现一个本地Map缓存（线程安全的）
+ *
+ * 缺点：
+ * 1.有可能造成写线程的锁饥饿问题
+ * 2.读的时候不能写, 只有读完成后才能写
  * @author: atong
  * @create: 2021-12-25 15:11
  */
