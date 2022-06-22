1ce3e2c5a8fcf6a46066a7eb6f8c359d0ba4856e
atong <atongLv@163.com>
2022-06-22 16:49:01 +0800

    feat: CompareToDemo

---
+++ src/main/java/datetime/CompareToDemo.java
@@ -0,0 +1,22 @@
+package datetime;
+
+import java.time.LocalDateTime;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @program: Draft
+ * @description: 比较大小
+ * @author: atong
+ * @create: 2022-06-20 17:36
+ */
+public class CompareToDemo {
+    public static void main(String[] args) throws InterruptedException {
+        LocalDateTime now1 = LocalDateTime.now();
+        System.out.println(now1);
+        TimeUnit.SECONDS.sleep(3);
+        LocalDateTime now2 = LocalDateTime.now();
+        System.out.println(now2);
+        int i = now1.compareTo(now2);
+        System.out.println(i);
+    }
+}
