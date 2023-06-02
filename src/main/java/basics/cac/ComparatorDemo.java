aaa64b6bbcd1d900af3ca1280ed444da47917ef6
atong <atongLv@163.com>
2023-06-02 09:27:37 +0800

    feat: ComparatorDemo

--- src/main/java/basics/cac/ComparatorDemo.java
+++ src/main/java/basics/cac/ComparatorDemo.java
@@ -17,7 +17,7 @@ import java.util.stream.Collectors;
 public class ComparatorDemo {
     public static void main(String[] args) {
         PersonC person1 = new PersonC("atong", 19);
-        PersonC person2 = new PersonC("yaoyao", 20);
+        PersonC person2 = new PersonC("zz", 20);
 
 
         List<PersonC> persons = new ArrayList<>();
@@ -44,5 +44,63 @@ public class ComparatorDemo {
         System.out.println(collect);
         System.out.println(collecta);
         System.out.println(persons);
+
+        System.out.println("============================");
+        List<PersonC> collect1 = persons.stream().sorted(Comparator.comparing(PersonC::getAge, (x,y) -> {
+            if(x == null && y != null){
+                return 1;
+            }else if(x !=null && y == null){
+                return -1;
+            }else if(x == null){
+                // x y 都为null
+                return 0;
+            }else {
+                if (x.equals(y)) {
+                    return 0;
+                } else if (x < y) {
+                    return 1;
+                } else {
+                    return -1;
+                }
+            }
+        })).collect(Collectors.toList());
+        System.out.println(collect1);
+        System.out.println("===============================");
+        persons.add(new PersonC("2",  2));
+        persons.add(new PersonC("3",  1));
+        persons.add(new PersonC("4",  0));
+        persons.add(new PersonC("5",  0));
+        persons.add(new PersonC("6",  2));
+        persons.add(new PersonC("7",  2));
+        persons.add(new PersonC("8",  1));
+        persons.add(new PersonC("9",  1));
+        persons.add(new PersonC("10",  1));
+        persons.add(new PersonC("11",  2));
+        List<PersonC> collect2 = persons.stream().sorted(Comparator.comparing(PersonC::getAge, (x,y) -> {
+            if(x == null && y != null){
+                return 1;
+            }else if(x !=null && y == null){
+                return -1;
+            }else if(x == null){
+                // x y 都为null
+                return 0;
+            }else {
+                if (x.equals(y)) {
+                    return 0;
+                }
+                if (x==1) {
+                    return -1;
+                }
+                if (x==2) {
+                    return 1;
+                }
+                if (y==1) {
+                    return 1;
+                } else {
+                    return -1;
+                }
+            }
+        }).thenComparing(PersonC::getName)).collect(Collectors.toList());
+        System.out.println(collect2);
     }
 }
